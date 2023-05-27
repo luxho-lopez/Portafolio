dcbdc00c15e9e6d26bb9c94fa76c6f485451a4f9
luxho-lopez <dj_97@outlook.com>
2023-05-26 21:52:22 -0600

    ✨

---
+++ js/db.js
@@ -0,0 +1,68 @@
+// PROYECTOS
+const proyectos = [
+    {
+        id: 1,
+        title: 'Proyecto 1',
+        image: 'img/proyectos/proyecto1.png',
+        github: 'https://github.com',
+        demo: 'https://github.com'
+    },
+    {
+        id: 2,
+        title: 'Proyecto 2',
+        image: 'img/proyectos/proyecto2.png',
+        github: 'https://github.com',
+        demo: 'https://github.com'
+    },
+    {
+        id: 3,
+        title: 'Proyecto 3',
+        image: 'img/proyectos/proyecto3.png',
+        github: 'https://github.com',
+        demo: 'https://github.com'
+    },
+    {
+        id: 4,
+        title: 'Proyecto 4',
+        image: 'img/proyectos/proyecto4.png',
+        github: 'https://github.com',
+        demo: 'https://github.com'
+    },
+    {
+        id: 5,
+        title: 'Proyecto 5',
+        image: 'img/proyectos/proyecto5.png',
+        github: 'https://github.com',
+        demo: 'https://github.com'
+    },
+    {
+        id: 6,
+        title: 'Proyecto 6',
+        image: 'img/proyectos/proyecto6.png',
+        github: 'https://github.com',
+        demo: 'https://github.com'
+    },
+];
+
+
+// TESTIMONIALS
+const data = [
+    {
+        avatar: 'img/testimonials/avatar1.jpg',
+        name: "Liliana",
+        review:
+            "Puedo decir que el tiempo que lo conozco es una persona confiable y vale la pena invertir un poco de tiempo y de $ en algo de suma importancia y con èl aprendes porque aprendes... El conocimiento que A obtenido hasta el día de hoy Puedo decir que es por su esfuerzo Y dedicación, Porque realmente Le apasiona lo que hace .... Mucha suerte en todo, sabes que Te Admiro desde siempre"
+    // },
+    // {
+    //     avatar: 'img/testimonials/avatar2.jpg',
+    //     name: "Deysi Mariela",
+    //     review:
+    //         "Es una persona muy profesional y sobre todo respetuoso. Puedo decir con toda confianza que él no juzga nuestras capacidades y conocimientos ya que esta abierto y disponible para el que quiera aprender siempre adecuándosea nuestras necesidades. Uno de los mejores maestros en cuanto a su perfil, yo lo conocí gracias a la recomendación de una persona cercana a mí. Y a parte de dar clases él se dedica a desarrollar otros trabajos por su propia cuenta. Espero le den y así mismo, ustedes se den la oportunidad de verificar lo que estoy redactando. Gracias."
+    // },
+    // {
+    //     avatar: 'img/testimonials/avatar3.jpg',
+    //     name: "Magiie",
+    //     review:
+    //         "Es buen amigo, lo conocí en la prepa y después de conocerlo aun más me di cuenta que tiene mucho talento, creo que seria un buen maestro por que sabe transmitir su conocimiente y es inteligente."
+    }
+];
--- js/main.js
+++ js/main.js
@@ -1,6 +1,71 @@
-const item = document.querySelector('.menu-item-link');
+const link = document.querySelectorAll('.menu__item-link');
 
-item.addEventListener('click', function(event) {
-    // event.preventDefault();
-    item.classList.add('active');
+
+link.forEach((el) => {
+    el.addEventListener('click', () => {
+        link.forEach((el) => {
+            el.classList.remove('active');
+        });
+        el.classList.add('active');
+    });
 });
+
+
+const resultado_proyecto = document.querySelector('#proyecto__item');
+
+document.addEventListener('DOMContentLoaded', () => {
+    mostrarProyectos(proyectos);
+});
+
+function mostrarProyectos(proyectos) {
+    proyectos.forEach(proyecto => {
+        const { id, title, image, github, demo } = proyecto;
+
+        const proyectoHTML = document.createElement('div');
+        proyectoHTML.classList.add('proyecto__item');
+        proyectoHTML.innerHTML = `
+            <div class="proyecto__item-img">
+                <img src="${image}" alt="${title}">
+            </div>
+            <div class="proyecto__item-info">
+                <h3>${title}</h3>
+                <div class="buttons">
+                    <a href="${github}" target="_blank"><button class="btn btn-light">Github</button></a>
+                    <a href="${demo}" target="_blank"><button class="btn btn-color">Live Demo</button></a>
+                </div>
+            </div>
+        `;
+        resultado_proyecto.appendChild(proyectoHTML);
+    });
+};
+
+
+// TESTIMONIALS
+const resultado_testimonial = document.querySelector('#testimonials__item');
+
+document.addEventListener('DOMContentLoaded', () => {
+    mostrarTestimonial(data);
+});
+
+function mostrarTestimonial(data) {
+    data.forEach(testimonial => {
+        const { avatar, name, review } = testimonial;
+
+        const testimonialHTML = document.createElement('div');
+        testimonialHTML.classList.add('testimonial__item');
+        testimonialHTML.innerHTML = `
+            <div class="testimonial__item-img">
+                <img src="${avatar}" alt="${name}">
+            </div>
+            <div class="testimonial__item-info">
+                <h4>${name}
+                    <span class="text-light">${review}<span>
+                </h4>
+            </div>
+        `;
+        resultado_testimonial.appendChild(testimonialHTML);
+    });
+};
+
+
+// SLIDER
